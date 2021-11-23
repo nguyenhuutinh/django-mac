d90954e5f18404a1f183357f81aa6f82b1208dc8
tinhnguyen <tn@ipification>
2021-11-23 23:42:20 +0700

    fix

--- backend/common/fshare_task.py
+++ backend/common/fshare_task.py
@@ -139,7 +139,8 @@ def parseCookieFile(cookiefile):
 
 
 def heartbeat():
-    threading.Timer(10.0, heartbeat).start()
+    thread = threading.Timer(10.0, heartbeat)
+    thread.start()
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     url_path = BASE_DIR + '/static/' + FILE_NAME
     jar = parseCookieFile(url_path)
@@ -150,15 +151,15 @@ def heartbeat():
         "x-requested-with":'XMLHttpRequest',
         'x-csrf-token':'A-fWA56bvXMQJOrRU1sfop--caYRmhadenW9lApzQ7A334Rhxt_fPVpluZMCPkXL2tce4XKtUfgJRNn5eD8x6Q==',
         'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
-
+        'Cookie':'_uidcms=1601864159616834536; fs_same_site=sameSite; _ga=GA1.2.504762787.1601864160; ajs_group_id=null; fpt_uuid=%22bf9df483-4a74-4479-aadd-b3f3a3c4cc6c%22; _gid=GA1.2.2097226903.1637504167; _fbp=fb.1.1637504167210.508549578; __gads=ID=be91e8f23e2a503a-22a775b833cf008b:T=1637504167:RT=1637504167:S=ALNI_MYMR59VmDHBm4UnIfTlwy6EjnHR0A; __yoid__=8941ce3caac7018bc56cdf633fcb411f; _ftitfsi=36353635343136; showPopupMemRegBeginTime=0a4a1bba24faf41c61d412300a499a137235ab8b05d3b508ba4a73313fc2e721a%3A2%3A%7Bi%3A0%3Bs%3A24%3A%22showPopupMemRegBeginTime%22%3Bi%3A1%3Bi%3A1637643277%3B%7D; showPopupMemRegLastTime=dcb2f05f2125f3d00d594301ae640d36674e7e73a689a450f436c87b1a786cb9a%3A2%3A%7Bi%3A0%3Bs%3A23%3A%22showPopupMemRegLastTime%22%3Bi%3A1%3Bi%3A1637643277%3B%7D; showPopupMemReg=fbef979f496638a917becd07db77635fbb7ecc3957e8dee12415ec2a2eb41024a%3A2%3A%7Bi%3A0%3Bs%3A15%3A%22showPopupMemReg%22%3Bi%3A1%3Bi%3A1%3B%7D; fshare-app=d9c7peshf5v2ermmd24hf8liu6; _csrf=wo_pV4R-C6AQ1JiJZAnJIaJadoaaXsDm'
     }
 
-    resp = requests.get('https://www.fshare.vn/site/motion-auth', cookies=jar, headers=headers_api)
+    resp = requests.get('https://www.fshare.vn/site/motion-auth', headers=headers_api)
     isSuccess = resp.json().get("success")
     print(resp.request.url)
     print(resp.request.body)
     print(resp.request.headers)
     if isSuccess != True:
-        threading.Timer(10.0, heartbeat).cancel()
+        thread.cancel()
     print(resp.json())
     return resp
