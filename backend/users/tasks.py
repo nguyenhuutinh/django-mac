dc263aa9969af9116b68263b2afdb7ed8a3fcb3b
Jack N <nguyenhuutinh88@gmail.com>
2022-07-07 21:20:03 +0700

    Update tasks.py

--- backend/users/tasks.py
+++ backend/users/tasks.py
@@ -18,10 +18,12 @@ def updateForms():
         sent=False,
         target_date__lte= datetime.now()
     )
-    print(scheduled_posts , datetime.now() + timedelta(seconds=1*10))
     if scheduled_posts != None and len(scheduled_posts) > 0 :
+
+
         formList = scheduled_posts
-        if len(scheduled_posts) > 0 :
+        if len(scheduled_posts) > 10 :
             formList = scheduled_posts[:10]
+        print(len(formList))
         for form in formList:
             googleSubmitForm.apply_async((form.auto_increment_id), countdown = 10)
