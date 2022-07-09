ff8f8acc3be985597c9c715c200362f808f16308
Jack N <nguyenhuutinh88@gmail.com>
2022-07-09 11:12:16 +0700

    Update base.py

--- backend/macos/settings/base.py
+++ backend/macos/settings/base.py
@@ -103,7 +103,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     # "django.contrib.sessions",
     # "django.contrib.messages",
-    # "django.contrib.staticfiles",
+    "django.contrib.staticfiles",
     # "django_js_reverse",
     # "webpack_loader",
     'knox',
