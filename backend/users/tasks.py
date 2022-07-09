77f25ff1054dcf32690e814cd4dbc034a9a1066f
Jack N <nguyenhuutinh88@gmail.com>
2022-07-10 01:51:53 +0700

    update

--- backend/users/tasks.py
+++ backend/users/tasks.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 import random
 from django.core import management
+from backend.common.models import Campaign
 from common.google_form_submit import googleSubmitForm
 from common.models import UserFormInfo
 
@@ -18,15 +19,17 @@ def updateForms():
     scheduled_posts = UserFormInfo.objects.filter(
         sent=False,
         target_date__lt = datetime.now()
-    ).order_by('auto_increment_id').select_related("campaign")
+    ).exclude(status='canceled', campaign__status = 'canceled').order_by('auto_increment_id').select_related("campaign")
     # print(scheduled_posts , datetime.now() + timedelta(seconds=1*10))
     if scheduled_posts != None and len(scheduled_posts) > 0 :
-
+        if scheduled_posts[0].campaign.status == "ready":
+            print("update status")
+            Campaign.objects.filter(id=scheduled_posts[0].campaign.id).update(status='running')
         formList = scheduled_posts
         if len(scheduled_posts) > 5 :
             formList = scheduled_posts[:5]
         print(f"available list {len(formList)}")
         for form in formList:
-            print(form)
-            print(form.campaign.id)
+            # print(form)
+            # print(form.campaign)
             googleSubmitForm.apply_async(kwargs={ "id":form.auto_increment_id}, countdown = random.randint(3, 30))
