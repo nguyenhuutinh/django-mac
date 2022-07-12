e02c0af5c25623f5c5c334e6c69be821fc56dbce
Jack N <nguyenhuutinh88@gmail.com>
2022-07-12 14:48:36 +0700

    Update celery.py

--- backend/macos/celery.py
+++ backend/macos/celery.py
@@ -22,6 +22,6 @@ app = Celery("macos")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 app.conf.beat_schedule = {
-    "form-every-120-seconds": {"schedule": 120.0, "task": "users.tasks.updateForms"},
+    "form-every-60-seconds": {"schedule": 60.0, "task": "users.tasks.updateForms"},
 }
 # print("hell")
