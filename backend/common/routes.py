c0dff1173d62498c01e9087a0935b9bc4c73c4c3
Jack N <nguyenhuutinh88@gmail.com>
2022-08-18 01:05:19 +0700

    ff

--- backend/common/routes.py
+++ backend/common/routes.py
@@ -1,8 +1,8 @@
-from .views import GoogleFormViewSet
+from .views import TeleGramBotViewSet
 
 routes = [
-    # {'regex': r'drive', 'viewset': GoogleDriveViewSet, 'basename': 'GoogleDrive'},
-    # {'regex': r'fshare', 'viewset': FshareViewSet, 'basename': 'Fshare'},
-    {'regex': r'google-form', 'viewset': GoogleFormViewSet, 'basename': 'GoogleForm'},
+    {'regex': r'telegram-bot', 'viewset': TeleGramBotViewSet, 'basename': 'TelegramBot'},
+    #{'regex': r'fshare', 'viewset': FshareViewSet, 'basename': 'Fshare'},
+    #{'regex': r'google-form', 'viewset': GoogleFormViewSet, 'basename': 'GoogleForm'},
     # {'regex': r'ads', 'viewset': AdsViewSet, 'basename': 'Fshare'},
 ]
