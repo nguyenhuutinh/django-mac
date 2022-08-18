43292b5bffb1220a2d07a33f971334772ef17412
Jack N <nguyenhuutinh88@gmail.com>
2022-08-18 22:18:16 +0700

    Update base.py

--- backend/macos/settings/base.py
+++ backend/macos/settings/base.py
@@ -143,7 +143,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 # "django.contrib.messages.context_processors.messages",
                 # "common.context_processors.sentry_dsn",
-                "common.context_processors.commit_sha",
+                # "common.context_processors.commit_sha",
             ],
             "loaders": [
                 (
