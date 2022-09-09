2c3601d8c7fcfc0cb368475fbda091892b637bb2
Jack N <nguyenhuutinh88@gmail.com>
2022-09-09 20:43:54 +0700

    Update tccl_bot.py

--- backend/common/tccl_bot.py
+++ backend/common/tccl_bot.py
@@ -148,7 +148,7 @@ def checkingUserProfilePhoto(message):
                 handle.write(block)
         file_exists = exists(filePath)
         if file_exists:
-            result = diff('/home/user/app/backend/data/logo1.jpg', filePath)
+            result = diff('/home/user/app/backend/data/logo1.jpg', filePath, diff_img_file='/home/user/app/backend/data/diff_img.png')
             print(result)
             result = compare_images(Image.open('/home/user/app/backend/data/logo1.jpg'), Image.open(filePath))
             print(result)
