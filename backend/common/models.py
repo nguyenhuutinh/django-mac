4bf05f35f97b66dc1cdfb32b1b905fabc816ec72
Jack N <nguyenhuutinh88@gmail.com>
2022-09-10 20:46:03 +0700

    Update models.py

--- backend/common/models.py
+++ backend/common/models.py
@@ -22,20 +22,20 @@ class IndexedTimeStampedModel(models.Model):
 #         "Campaign", on_delete=models.CASCADE)
 class TelegramUser(models.Model):
     id = models.AutoField(primary_key=True)
-    uid = models.CharField(max_length=300, default="" )
+    user_id = models.CharField(max_length=300, default="" )
     firstname = models.CharField(max_length=300, default="", null=True,  )
     lastname = models.CharField(max_length=300, default="" , null=True)
     username = models.CharField(max_length=300, default="", null=True )
     status = models.CharField(max_length=300, default="" )
     ban_reason = models.CharField(max_length=300, default="" )
-    message = models.ForeignKey(
-        "Message", on_delete=models.CASCADE)
+
     isBot  = models.BooleanField(default=False)
     user_avatar_link = models.CharField(max_length=300, default="" , null=True, )
 
 class Message(models.Model):
     message_id = models.CharField(max_length=200 ,  primary_key=True)
-    user_id = models.CharField(max_length=200 )
+    user = models.ForeignKey(
+        "TelegramUser", on_delete=models.CASCADE)
     text = models.CharField(max_length=300, default="", null=True,  )
     date_timestamp = models.CharField(max_length=300, default="" )
     status = models.CharField(max_length=300, default="" )
