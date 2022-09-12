6e06cdf885be07292d53a06e8a449ff54f9dcb8c
Jack N <nguyenhuutinh88@gmail.com>
2022-09-12 18:06:38 +0700

    Update manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -16,7 +16,9 @@ class IsAdmin(telebot.custom_filters.SimpleCustomFilter):
     key='is_admin'
     @staticmethod
     def check(message: telebot.types.Message):
-        return bot.get_chat_member(message.chat.id,message.from_user.id).status in ['administrator','creator']
+        result = bot.get_chat_member(message.chat.id,message.from_user.id).status in ['administrator','creator']
+        print("check" + result)
+        return result
 
 
 
