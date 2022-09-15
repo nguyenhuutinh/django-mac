697f10edfc20faef110a726d850e032d1864b94b
Jack N <nguyenhuutinh88@gmail.com>
2022-09-15 09:19:18 +0700

    Update urls.py

--- backend/common/urls.py
+++ backend/common/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path
 
-from . import views, views_google_docs
+from . import views
 
 app_name = 'common'
 urlpatterns = [
     # path('', views.IndexView.as_view(), name='index'),
     # path('drive/<str:file_name>', views.IndexView.as_view(), name='index'),
     # path('fshare', views.IndexView.as_view(), name='index'),
-    path('api/google-form', views_google_docs.GoogleFormViewSet.as_view()),
+    # path('api/google-form', views_google_docs.GoogleFormViewSet.as_view()),
     # path('ads', views.IndexView.as_view(), name='index'),
     # path('tccl-bot', views.IndexView.as_view(), name='index'),
     path('api/tccl-admin/users', views.UsersApi.as_view()),
