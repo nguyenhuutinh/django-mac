69299ffa430f2cd82fee329124f77496636f06bd
Jack N <nguyenhuutinh88@gmail.com>
2022-09-26 13:49:41 +0700

    Update routes.py

--- backend/common/routes.py
+++ backend/common/routes.py
@@ -1,7 +1,7 @@
 from .views import TCCLBotView, UsersApi
 from .views_google_docs import GoogleFormViewSet
 routes = [
-    {'regex': r'tccl-bot', 'viewset': TCCLBotView, 'basename': 'TCCLBotView'},
+    # {'regex': r'tccl-bot', 'viewset': TCCLBotView, 'basename': 'TCCLBotView'},
     # {'regex': r'tccl-admin', 'viewset': UsersApi, 'basename': 'UsersApi'},
 
     #{'regex': r'fshare', 'viewset': FshareViewSet, 'basename': 'Fshare'},
