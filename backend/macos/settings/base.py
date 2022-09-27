6d2b56ce5605338bc7ec3bec31c9bedebb7d856e
Jack N <nguyenhuutinh88@gmail.com>
2022-09-27 10:18:46 +0700

    Update base.py

--- backend/macos/settings/base.py
+++ backend/macos/settings/base.py
@@ -119,17 +119,19 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
 
     "debreach.middleware.RandomCommentMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.common.CommonMiddleware",
+
+
 ]
 
 ROOT_URLCONF = "macos.urls"
