c81c481100348dc54463a40615048501cd86f7f0
Nguyen Huu Tinh <nguyenhuutinh@Tinh-Nguyens-MacBook-Pro.local>
2022-12-29 02:51:11 +0700

    f

--- backend/telegrambot/celery.py
+++ backend/telegrambot/celery.py
@@ -25,4 +25,4 @@ app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 # app.conf.beat_schedule = {
 #     "form-every-60-seconds": {"schedule": crontab(minute="*/1"), "task": "users.tasks.updateForms"},
 # }
-print("hell")
+# print("hell")
