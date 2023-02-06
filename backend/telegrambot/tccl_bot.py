72111800f0d3085fd4252df7265325744dbff54e
Nguyen Huu Tinh <nguyenhuutinh@Tinh-Nguyens-MacBook-Pro.local>
2023-02-06 20:22:13 +0700

    Update tccl_bot.py

--- backend/telegrambot/tccl_bot.py
+++ backend/telegrambot/tccl_bot.py
@@ -427,9 +427,9 @@ def allMessage(message):
         return
     print(f"\n{bcolors.UNDERLINE}{bcolors.OKCYAN}{message.from_user.first_name} sent message:  {str( message.text)} {bcolors.ENDC} {warning_count}\n")
     warning_count += 1
-    if warning_count >= warning_max:
-        warning_count = 0
-        bot.reply_to(message, "‼️ 🆘💢 Cảnh báo lừa đảo 💢🆘 ‼️\n\n👉 ⚠️TCCL KHÔNG có group VIP.\n👉 ⚠️TCCL KHÔNG thu khoản phí nào.\n👉 ⚠️Các admin KHÔNG bao giờ DM trước.\n👉 Hãy luôn cẩn thận với tài sản của mình.🦮🦮🦮")
+    # if warning_count >= warning_max:
+    #     warning_count = 0
+    #     bot.reply_to(message, "‼️ 🆘💢 Cảnh báo lừa đảo 💢🆘 ‼️\n\n👉 ⚠️TCCL KHÔNG có group VIP.\n👉 ⚠️TCCL KHÔNG thu khoản phí nào.\n👉 ⚠️Các admin KHÔNG bao giờ DM trước.\n👉 Hãy luôn cẩn thận với tài sản của mình.🦮🦮🦮")
 
     moderate(message=message)
 
