3b2179b81c426320f0f9a80c55f349717075547c
Nguyen Huu Tinh <nguyenhuutinh@Tinh-Nguyens-MacBook-Pro.local>
2023-04-05 14:21:05 +0700

    Update views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -67,45 +67,45 @@ class RegisterApi(generics.GenericAPIView):
         print("haha")
 
         # Open the image file
-        img = Image.open('/Users/nguyenhuutinh/Projects/MyProjects/GitHub/django-mac/backend/data/test2.jpg')
+        # img = Image.open('/Users/nguyenhuutinh/Projects/MyProjects/GitHub/django-mac/backend/data/test2.jpg')
 
-        # Use pytesseract to convert the image to text
-        text = pytesseract.image_to_string(img)
-        pattern = r'\b(1[0-9]{3,}|[2-9][0-9]{3,})\.\d*%|\b(1[0-9]{3,}|[2-9][0-9]{3,})%'
+        # # Use pytesseract to convert the image to text
+        # text = pytesseract.image_to_string(img)
+        # pattern = r'\b(1[0-9]{3,}|[2-9][0-9]{3,})\.\d*%|\b(1[0-9]{3,}|[2-9][0-9]{3,})%'
 
-        # Use the regex search function to find any percentage value greater than or equal to 1000 in the text
-        match = re.search(pattern, text)
+        # # Use the regex search function to find any percentage value greater than or equal to 1000 in the text
+        # match = re.search(pattern, text)
 
-        # If a percentage value greater than or equal to 1000 is found, print it
-        if match:
-            print(f"Percentage value greater than or equal to 1000 found: {match.group(0)}")
+        # # If a percentage value greater than or equal to 1000 is found, print it
+        # if match:
+        #     print(f"Percentage value greater than or equal to 1000 found: {match.group(0)}")
 
 
-        # Load the image file
-        img = cv2.imread('/Users/nguyenhuutinh/Projects/MyProjects/GitHub/django-mac/backend/data/test-2.jpg')
+        # # Load the image file
+        # img = cv2.imread('/Users/nguyenhuutinh/Projects/MyProjects/GitHub/django-mac/backend/data/test-2.jpg')
 
-        # Load the pre-trained neural network model for nudity detection
-        model = cv2.dnn.readNetFromCaffe('/Users/nguyenhuutinh/Projects/MyProjects/GitHub/django-mac/backend/data/deploy.prototxt', '/Users/nguyenhuutinh/Projects/MyProjects/GitHub/django-mac/backend/data/res10_300x300_ssd_iter_140000.caffemodel')
+        # # Load the pre-trained neural network model for nudity detection
+        # model = cv2.dnn.readNetFromCaffe('/Users/nguyenhuutinh/Projects/MyProjects/GitHub/django-mac/backend/data/deploy.prototxt', '/Users/nguyenhuutinh/Projects/MyProjects/GitHub/django-mac/backend/data/res10_300x300_ssd_iter_140000.caffemodel')
 
-        # Define the classes for the neural network model
-        classes = ['background', 'person']
-        if img is None or img.shape[0] == 0 or img.shape[1] == 0:
-            raise ValueError('Invalid input image')
-        # Convert the image to a blob
-        input_size = (300, 300)
+        # # Define the classes for the neural network model
+        # classes = ['background', 'person']
+        # if img is None or img.shape[0] == 0 or img.shape[1] == 0:
+        #     raise ValueError('Invalid input image')
+        # # Convert the image to a blob
+        # input_size = (300, 300)
 
-        blob = cv2.dnn.blobFromImage(img, 1.0, input_size, (104.0, 177.0, 123.0))
+        # blob = cv2.dnn.blobFromImage(img, 1.0, input_size, (104.0, 177.0, 123.0))
 
-        # Set the input for the neural network model
-        model.setInput(blob)
+        # # Set the input for the neural network model
+        # model.setInput(blob)
 
-        # Perform forward pass to get the output from the neural network model
-        output = model.forward()
+        # # Perform forward pass to get the output from the neural network model
+        # output = model.forward()
 
-        # Check if any of the detected objects are classified as a person
-        for i in range(output.shape[2]):
-            confidence = output[0, 0, i, 2]
-            if confidence > 0.5 and classes[int(output[0, 0, i, 1])] == 'person':
-                print('Nudity detected')
-                break
-        return Response({"hello":text})
+        # # Check if any of the detected objects are classified as a person
+        # for i in range(output.shape[2]):
+        #     confidence = output[0, 0, i, 2]
+        #     if confidence > 0.5 and classes[int(output[0, 0, i, 1])] == 'person':
+        #         print('Nudity detected')
+        #         break
+        return Response({"hello":"text"})
