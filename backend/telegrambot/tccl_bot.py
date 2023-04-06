14af96bf0ddf2ac580e172b8028183d97427ac4b
Nguyen Huu Tinh <nguyenhuutinh@Tinh-Nguyens-MacBook-Pro.local>
2023-04-06 10:13:07 +0700

    Update tccl_bot.py

--- backend/telegrambot/tccl_bot.py
+++ backend/telegrambot/tccl_bot.py
@@ -102,17 +102,16 @@ def photo(message):
     if res == 1:
         userId = message.from_user.id
         chatId = message.chat.id
-        bot.ban_chat_member(chatId, userId)
         deleteMessageTask.apply_async(kwargs={ "chat_id": chatId,'message_id': message.message_id}, countdown=3)
 
-
-        bot.reply_to(message, "‼️ CHIM CÚT ‼️" + "\n\n👉 ⚠️TCCL KHÔNG có group VIP.\n👉 ⚠️TCCL KHÔNG THU khoản phí nào.\n👉 ⚠️Các admin KHÔNG BAO GIỜ NHẮN TIN trước.\n👉 ⚠️ Bất kỳ ai đều có thể đổi tên và avatar giống admin để chat với bạn\n👉 Hãy luôn CẨN THẬN với tài sản của mình.")
+        # bot.ban_chat_member(chatId, userId)
+        bot.reply_to(message, full_name + " bị ban vì post hình ảnh có nội dung SCAM / LỪA ĐẢO. ‼️" + "\n\n👉 ⚠️TCCL KHÔNG có group VIP.\n👉 ⚠️TCCL KHÔNG THU khoản phí nào.\n👉 ⚠️Các admin KHÔNG BAO GIỜ NHẮN TIN trước.\n👉 ⚠️ Bất kỳ ai đều có thể đổi tên và avatar giống admin để chat với bạn\n👉 Hãy luôn CẨN THẬN với tài sản của mình.")
         bot.send_message("-1001349899890", "SCAM-HÌNH ẢNH - Đã ban user id: " + str(userId) + " - "+ f"{full_name}" + f" - message: {message.id} {message.text} " + f" - caption: {message.caption}")
     elif res == 2:
         userId = message.from_user.id
         chatId = message.chat.id
         deleteMessageTask.apply_async(kwargs={ "chat_id": chatId,'message_id': message.message_id}, countdown=3)
-        bot.reply_to(message, "‼️ Đăng hình 3X là ra dảo nha ‼️")
+        bot.reply_to(message, "‼️ Tin nhắn bị xóa / " + full_name + " sử dụng hình ảnh 3X. ‼️")
         bot.send_message("-1001349899890", "SPAM ẢNH SEX - user id: " + str(userId) + " - "+ f"{full_name}" + f" - message: {message.id} {message.text} " + f" - caption: {message.caption}")
     else:
         print("check photo and it is valid")
