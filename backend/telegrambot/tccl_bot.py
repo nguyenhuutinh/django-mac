6581584729699c2219b378f166161e9c0ed33dcc
Nguyen Huu Tinh <nguyenhuutinh@Tinh-Nguyens-MacBook-Pro.local>
2023-04-14 00:10:13 +0700

    Update tccl_bot.py

--- backend/telegrambot/tccl_bot.py
+++ backend/telegrambot/tccl_bot.py
@@ -275,7 +275,9 @@ def checkingPhoto(message):
         print(file_exists)
         if file_exists:
 
-            img = cv2.imread(filePath, 1)
+            # img = cv2.imread(filePath, 1)
+            img = Image.open(filePath)
+
 
             # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
