846f8293f1d24eb3519d163e06c00821ee3587a0
Nguyen Huu Tinh <nguyenhuutinh@Tinh-Nguyens-MacBook-Pro.local>
2023-07-06 13:09:04 +0700

    Update tccl_bot.py

--- backend/telegrambot/tccl_bot.py
+++ backend/telegrambot/tccl_bot.py
@@ -650,15 +650,15 @@ def processCheckAndBan(message):
     if "TCCL".lower() in f"{firstName} {lastName}".lower():
         print(f"{bcolors.WARNING}case 5  {bcolors.ENDC}")
         return True
-    if ".".lower() == f"{firstName}".lower() and None == lastName:
-        print(f"{bcolors.WARNING}case 6  {bcolors.ENDC}")
-        return True
-    if "..".lower() == f"{firstName}".lower() and None == lastName:
-        print(f"{bcolors.WARNING}case 7  {bcolors.ENDC}")
-        return True
-    if "...".lower() == f"{firstName}".lower() and None == lastName:
-        print(f"{bcolors.WARNING}case 8  {bcolors.ENDC}")
-        return True
+    # if ".".lower() == f"{firstName}".lower() and None == lastName:
+    #     print(f"{bcolors.WARNING}case 6  {bcolors.ENDC}")
+    #     return True
+    # if "..".lower() == f"{firstName}".lower() and None == lastName:
+    #     print(f"{bcolors.WARNING}case 7  {bcolors.ENDC}")
+    #     return True
+    # if "...".lower() == f"{firstName}".lower() and None == lastName:
+    #     print(f"{bcolors.WARNING}case 8  {bcolors.ENDC}")
+    #     return True
     if "tccl" in f"{username}":
         print(f"{bcolors.WARNING}case 9  {bcolors.ENDC}")
         return True
