2593bfd2bd62d1a21a9719634f59abd94c4bca97
Nguyen Huu Tinh <nguyenhuutinh@Tinh-Nguyens-MacBook-Pro.local>
2023-08-25 14:47:22 +0700

    Update tccl_bot.py

--- backend/telegrambot/tccl_bot.py
+++ backend/telegrambot/tccl_bot.py
@@ -570,6 +570,8 @@ def checkAndDeleteMessage(message):
         return True
     if "@RyanNguyenVBC" in f"{message.text} {message.caption}":
         return True
+    if "vinacoin.live" in f"{message.text} {message.caption}":
+        return True
     # if "mẹ" in f"{message.text} {message.caption}".lower() and "mày" in f"{message.text} {message.caption}".lower():
     #     print(f"{bcolors.WARNING}case 5  {bcolors.ENDC}")
     #     return True
