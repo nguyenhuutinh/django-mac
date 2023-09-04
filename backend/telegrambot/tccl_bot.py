23cd50f6e5fbc26c24930161a5e46aa4892543c5
Nguyen Huu Tinh <nguyenhuutinh@Tinh-Nguyens-MacBook-Pro.local>
2023-09-04 15:41:01 +0700

    Update tccl_bot.py

--- backend/telegrambot/tccl_bot.py
+++ backend/telegrambot/tccl_bot.py
@@ -33,7 +33,7 @@ api_instance = cloudmersive_image_api_client.NsfwApi(cloudmersive_image_api_clie
 
 photoUrl = ""
 MSG_COUNTER = 0
-MSG_MAX = 100
+MSG_MAX = 20
 
 # from PIL import ImageChops, ImageStat,Image
 
