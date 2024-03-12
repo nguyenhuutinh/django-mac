07e67d27ccfa111b618fb8f457ae5dd50514b670
Nguyen Huu Tinh <nguyenhuutinh@Tinh-Nguyens-MacBook-Pro.local>
2024-03-12 22:49:27 +0700

    Update tccl_bot.py

--- backend/telegrambot/tccl_bot.py
+++ backend/telegrambot/tccl_bot.py
@@ -525,7 +525,10 @@ def moderate(message):
         if message.text == None:
             isPhoto = True
         if isExist is not True and isPhoto is True and message.caption is not None:
-            print(f"NOT exist but user sent image with caption . User ID: {message.from_user.id}. Delete message")
+            print(f"new user but user sent image with caption . User ID: {message.from_user.id}. Delete message")
+            _deleteMessage(message)
+        if isExist is not True and "https://" in f"{message.text} {message.caption}".lower():
+            print(f"new user but user sent link . User ID: {message.from_user.id}. Delete message")
             _deleteMessage(message)
 
         if isExist is not True or isPhoto is True:
